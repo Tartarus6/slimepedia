61561a31e31af7083f84c4e36897585600de44dc
Tartarus6 <ivanglott@gmail.com>
2025-11-08 08:08:52 +0100

    moved darkreader-lock to app.html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -37,7 +37,6 @@
 	<title>Slimepedia</title>
 	<link rel="icon" type="image/png" href="/favicon.png" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="darkreader-lock">
 </svelte:head>
 
 
